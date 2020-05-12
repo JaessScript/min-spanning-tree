baa668e26c56437eb571bd47991b087f56cc3a56
jaess-script <jaess.script@gmail.com>
2020-05-12 11:28:17 +0200

    Initialise the problem

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+let vertices = [];
+let unreached = [];
+let reached = [];
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 }
 
-function draw() {
+function mousePressed() {
+	let v = createVector(mouseX, mouseY);
+	vertices.push(v);
+}
 
+function draw() {
+	background(30);
+
+	for (let i = 0; i < vertices.length; i++) {
+		fill(255);
+		stroke(255);
+		ellipse(vertices[i].x, vertices[i].y, 16, 16);
+	}
 }
