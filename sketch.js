b4e94d7e499cd724432b88c8a85631a6d3314316
jaess-script <jaess.script@gmail.com>
2020-05-12 12:48:11 +0200

    Solve the problem, core algorithm

--- sketch.js
+++ sketch.js
@@ -1,6 +1,4 @@
 let vertices = [];
-let unreached = [];
-let reached = [];
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -14,9 +12,44 @@ function mousePressed() {
 function draw() {
 	background(30);
 
+	let unreached = [];
+	let reached = [];
+
 	for (let i = 0; i < vertices.length; i++) {
 		fill(255);
 		stroke(255);
 		ellipse(vertices[i].x, vertices[i].y, 16, 16);
 	}
+
+	// At first, all vertices are unreached
+	for (let i = 0; i < vertices.length; i++) {
+		unreached.push(vertices[i]);
+	}
+
+	// Let's randomly start considering the vertices[0]
+	reached.push(vertices[0]);
+	unreached.splice(0, 1);
+
+	// As long as they are unreached vertices, we keep on searching for the smallest distance
+	while (unreached.length > 0) {
+		let record = Infinity;
+		let rIndex;
+		let uIndex;
+
+		for (let i = 0; i < reached.length; i++) {
+			for (let j = 0; j < unreached.length; j++) {
+				let d = dist(reached[i].x, reached[i].y, unreached[j].x, unreached[j].y);
+				if (d < record) {
+					record = d;
+					rIndex = i;
+					uIndex = j;
+				}
+			}
+		}
+
+		line(reached[rIndex].x, reached[rIndex].y, unreached[uIndex].x, unreached[uIndex].y);
+		reached.push(unreached[uIndex]);
+		unreached.splice(uIndex, 1);
+	}
+
 }
